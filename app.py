e4479d0bd9ba0a48b83d03cf6a2e0704fea35172
andrelg3 <andrelg3@gmail.com>
2026-08-01 22:46:19 -0400

    Fix formatting in month options list

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ with st.sidebar:
             "05 - MAIO", "06 - JUNHO", "07 - JULHO", 
             "08 - AGOSTO", "09 - SETEMBRO", "10 - OUTUBRO", 
             "11 - NOVEMBRO", "12 - DEZEMBRO"
-        ]
+            ]
     with col_ano:
     ano_ref = st.selectbox("Ano", ["2026", "2025"])
         vigencia_ref = st.selectbox("Mês", meses_opcoes, index=4)
