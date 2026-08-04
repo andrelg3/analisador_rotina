8c215a429aa92b68a4d07b544c160a45469804bc
andrelg3 <andrelg3@gmail.com>
2026-08-04 09:27:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ with st.sidebar:
             "06 - JUNHO", "07 - JULHO", 
             "08 - AGOSTO", "09 - SETEMBRO"
         ]
-        vigencia_ref = st.selectbox("Mês", meses_opcoes, index=4)
+        vigencia_ref = st.selectbox("Mês", meses_opcoes, index=7)
     with col_ano:
         ano_ref = st.selectbox("Ano", ["2026", "2025"])
 
